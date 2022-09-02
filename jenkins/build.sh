c3e7fb9db14c0a4970b51ac556fdd484c880dbb2
ponnethali <112630760+ponnethali@users.noreply.github.com>
2022-09-02 17:02:26 +0530

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cp /home/azureuser/index.html .
 touch Dockerfile
 echo "  
 FROM nginx:alpine
-COPY sample/index.html /usr/share/nginx/html">>Dockerfile
+COPY index.html /usr/share/nginx/html">>Dockerfile
 docker build -t aliponneth/app:latest .
